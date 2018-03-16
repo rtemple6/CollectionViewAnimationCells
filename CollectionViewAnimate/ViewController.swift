5a5022e0f8cdd4180d0fc2e2314ece6f011a320a
Ryan <rtemple@ramapo.edu>
2018-03-16 17:19:25 -0400

    Initial Commit

---
+++ CollectionViewAnimate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewAnimate
+//
+//  Created by Ryan Temple on 3/16/18.
+//  Copyright © 2018 Ryan Temple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
